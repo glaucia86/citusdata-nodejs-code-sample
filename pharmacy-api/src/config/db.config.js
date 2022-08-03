90f6ed24c7e1587ed55cd5f45ffa264738d55499
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-03 11:56:51 -0300

    creating the 'db.config.js' file database connection

--- pharmacy-api/src/config/db.config.js
+++ pharmacy-api/src/config/db.config.js
@@ -4,3 +4,22 @@
  * data: 08/03/2022
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
+
+const { Pool } = require('pg');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// ==> Database connection
+const pool = new Pool({
+  connectionString: process.env.CITUS_DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('Database connected with success!');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
