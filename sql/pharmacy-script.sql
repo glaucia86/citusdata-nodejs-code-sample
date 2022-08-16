9b91a86436f1c2ada22978a22e259711191bb9b2
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-16 12:18:56 -0300

    modified pharmacy-script.sql'

--- sql/pharmacy-script.sql
+++ sql/pharmacy-script.sql
@@ -1,6 +1,6 @@
-CREATE TABLE pharmacy (
-  pharmacy_id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  pharmacy_name VARCHAR(255) NOT NULL,
+CREATE TABLE pharmacies (
+  id uuid PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   state VARCHAR(255) NOT NULL,
   zip_code INTEGER NOT NULL
